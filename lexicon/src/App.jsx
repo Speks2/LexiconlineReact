dca4febbc1602a9cb70a83beaaa5430eb0568218
Speks2 <142408255+Speks2@users.noreply.github.com>
2024-09-06 09:27:42 +0200

    getting SCSS ready

--- lexicon/src/App.jsx
+++ lexicon/src/App.jsx
@@ -1,10 +1,18 @@
 import { Lexicon } from "./components/Lexicon/Lexicon"
+import { Header }  from "./components/Header/Header"
+import { Main } from "./components/Main/Main";
+import { Footer } from "./components/Footer/Footer";
 
 function App() {
 
   return (
     <>
+    <Header />
     <Lexicon />
+    <Main/>
+    <Footer />
+    
+    
   </>
   )
 }
