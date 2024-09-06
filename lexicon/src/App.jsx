dc06a014ac42f8eb4528c7afac6c7265a469f4a2
Speks2 <142408255+Speks2@users.noreply.github.com>
2024-09-06 09:49:37 +0200

    trying to fix SCSS

--- lexicon/src/App.jsx
+++ lexicon/src/App.jsx
@@ -2,6 +2,7 @@ import { Lexicon } from "./components/Lexicon/Lexicon"
 import { Header }  from "./components/Header/Header"
 import { AboutText } from "./components/Main/AboutText";
 import { Footer } from "./components/Footer/Footer";
+import "/src/index.scss";
 
 function App() {
 
