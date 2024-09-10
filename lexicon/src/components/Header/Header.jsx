78297be97108352b0fb65fd4181f9bedb381dd4a
Speks2 <142408255+Speks2@users.noreply.github.com>
2024-09-10 12:39:36 +0200

    trying to fix the h1

--- lexicon/src/components/Header/Header.jsx
+++ lexicon/src/components/Header/Header.jsx
@@ -14,15 +14,19 @@ export function Header() {
         <img src="/src/assets/images/Logo.svg" alt="headerSvg" />
         </div>
         <h1>Lexiconline</h1>
-        <container><h2>About Lexiconline</h2></container>
+        <div className={styles.About}><h2>About Lexiconline</h2></div>
         <div className={styles.Navbar}>
         <nav>
-         <a href="#home" className={styles.navItem}>HOME</a>
+          <div>
+          <a href="#home" className={styles.navItem}>HOME</a>
          <a href="#about" className={styles.navItem}>ABOUT</a>
          <a href="#api" className={styles.navItem}>API</a>
+          </div>
         </nav>
         </div>
+        <div className={styles.HeaderImage}>
         <img src="/src/assets/images/headerImage.jpg" alt="headerImage"/>
+        </div>
       </header>
      
        </>
